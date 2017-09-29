bc13950cc4d66a3bc9dcd7a7a71ce2aaf230ea20
brendon.barreto <brendondominic@gmail.com>
2017-09-29 13:08:21 -0300

    decreasing timer

--- smarttimer.js
+++ smarttimer.js
@@ -26,7 +26,7 @@ class SmartTimerUtils {
 		var milliseconds = parseInt(ms % 1000);
 		var seconds = parseInt((ms / 1000) % 60);
 		var minutes = parseInt(((ms / (1000 * 60)) % 60));
-		var hours = parseInt(((ms / (1000 * 60 * 60)) % 24));
+		var hours = Math.floor(ms / (1000 * 60 * 60));
 
 		return new SmartTimerTime({
 			milliseconds: milliseconds,
@@ -35,21 +35,37 @@ class SmartTimerUtils {
 			hours: hours
 		});
 	}
+
+	static padLeft(num, padSize = 2) {
+		const str = num.toString();
+		const pad = "0".repeat(padSize);
+		const ans = pad.substring(0, pad.length - str.length) + str;
+		return ans;
+	}
 }
 
 class SmartTimerTime {
 	constructor(time) {
-		this.milliseconds = time.milliseconds;
-		this.seconds = time.seconds;
-		this.minutes = time.minutes;
-		this.hours = time.hours;
+		this.milliseconds = time.milliseconds || 0;
+		this.seconds = time.seconds || 0;
+		this.minutes = time.minutes || 0;
+		this.hours = time.hours || 0;
+	}
+
+	toString() {
+		return SmartTimerUtils.padLeft(this.hours) + ":" + SmartTimerUtils.padLeft(this.minutes)
+			+ ":" + SmartTimerUtils.padLeft(this.seconds) + ":" + SmartTimerUtils.padLeft(this.milliseconds, 3);
 	}
 }
 
 class SmartTimer {
 	constructor(settings) {
 		this.settings = settings;
+		this.settings.interval = this.settings.interval || new SmartTimerTime({ seconds: 1 });
+		this.settings.startTime = this.settings.startTime || new SmartTimerTime({});
 		this._currentTime = this._getStartMs();
+		this._currentSmartTime = SmartTimerUtils.msToSmartTime(this._currentTime);
+
 		this._endTimeMs = this.settings.endTime ? SmartTimerUtils.toMilliseconds(this.settings.endTime) : 0;
 	}
 
@@ -64,7 +80,7 @@ class SmartTimer {
 			specificTimes.forEach((st) => {
 				var ms = SmartTimerUtils.toMilliseconds(st.when);
 				if (ms == $this._currentTime) {
-					st.event(ms);
+					st.event($this._currentSmartTime);
 				}
 			});
 		}
@@ -78,7 +94,7 @@ class SmartTimer {
 			intervals.forEach((interval) => {
 				var ms = SmartTimerUtils.toMilliseconds(interval.interval);
 				if (($this._currentTime - startTime) % ms == 0) {
-					interval.event($this._currentTime);
+					interval.event($this._currentSmartTime);
 				}
 			});
 		}
@@ -88,22 +104,23 @@ class SmartTimer {
 		var $this = this;
 		if (this.state != SmartTimerState.STARTED) {
 			this.state = SmartTimerState.STARTED;
-			$this.settings.onStart && $this.settings.onStart(this._currentTime);
+			$this.settings.onStart && $this.settings.onStart(this._currentSmartTime);
 
 			var ms = SmartTimerUtils.toMilliseconds(this.settings.interval);
 			this._intervalInstance = setInterval(function () {
-				$this._currentTime = $this._currentTime + ms;
+				$this._currentTime = $this.settings.decreasing ? ($this._currentTime - ms) : ($this._currentTime + ms);
+				$this._currentSmartTime = SmartTimerUtils.msToSmartTime($this._currentTime);
 				$this._runSpecificTimes();
 				$this._runIntervals();
 
 
-				if ($this.settings.endTime && $this._currentTime == $this._endTimeMs) {
+				if (($this.settings.endTime && $this._currentTime == $this._endTimeMs) || ($this.settings.decreasing && $this._currentTime == 0)) {
 					$this.end();
 				} else {
-					$this.settings.onInterval && $this.settings.onInterval($this._currentTime);
+					$this.settings.onInterval && $this.settings.onInterval($this._currentSmartTime);
 				}
 
-				
+
 			}, ms);
 		}
 	}
@@ -111,8 +128,10 @@ class SmartTimer {
 	end() {
 		if (this.state != SmartTimerState.ENDED) {
 			this.state = SmartTimerState.ENDED;
-			this.settings.onEnd && this.settings.onEnd(this._currentTime);
+			this.settings.onEnd && this.settings.onEnd(this._currentSmartTime);
+
 			this._currentTime = this._getStartMs();
+			this._currentSmartTime = SmartTimerUtils.msToSmartTime(this._currentTime);
 			clearInterval(this._intervalInstance);
 		}
 	}
@@ -120,7 +139,7 @@ class SmartTimer {
 	pause() {
 		if (this.state != SmartTimerState.PAUSED) {
 			this.state = SmartTimerState.PAUSED;
-			this.settings.onPause && this.settings.onPause(this._currentTime);
+			this.settings.onPause && this.settings.onPause(this._currentSmartTime);
 			clearInterval(this._intervalInstance);
 		}
 
@@ -129,98 +148,11 @@ class SmartTimer {
 	stop() {
 		if (this.state != SmartTimerState.STOPED) {
 			this.state = SmartTimerState.STOPED;
-			this.settings.onStop && this.settings.onStop(this._currentTime);
+			this.settings.onStop && this.settings.onStop(this._currentSmartTime);
+
 			this._currentTime = this._getStartMs();
+			this._currentSmartTime = SmartTimerUtils.msToSmartTime(this._currentTime);
 			clearInterval(this._intervalInstance);
 		}
 	}
 }
-
-var timer = new SmartTimer({
-	startTime: new SmartTimerTime({
-		milliseconds: 0,
-		seconds: 5,
-		minutes: 0,
-		hours: 0
-	}),
-	interval: new SmartTimerTime({
-		milliseconds: 500,
-		seconds: 0,
-		minutes: 0,
-		hours: 0
-	}),
-	onStart: (currentTime) => {
-		console.log("inicio em " + currentTime);
-	},
-	onEnd: (currentTime) => {
-		console.log("termino em " + currentTime);
-	},
-	onPause: (currentTime) => {
-		console.log("pauso em " + currentTime);
-	},
-	onStop: (currentTime) => {
-		console.log("stopo em " + currentTime);
-	},
-	onInterval: (currentTime) => {
-		console.log("tá ino em " + currentTime);
-	},
-	onSpecificTimes: [
-		{
-			when: {
-				milliseconds: 0,
-				seconds: 6,
-				minutes: 0,
-				hours: 0
-			},
-			event: (currentTime) => {
-				console.log("st em " + currentTime);
-			}
-		},
-		{
-			when: {
-				milliseconds: 0,
-				seconds: 9,
-				minutes: 0,
-				hours: 0
-			},
-			event: (currentTime) => {
-				console.log("st em " + currentTime);
-			}
-		}
-	],
-	onIntervals: [
-		{
-			interval: {
-				milliseconds: 0,
-				seconds: 3,
-				minutes: 0,
-				hours: 0
-			},
-			event: (currentTime) => {
-				console.log("it em " + currentTime);
-			}
-		},
-	]
-});
-
-//console.log(SmartTimerUtils.toMilliseconds({
-//	milliseconds: 16,
-//	seconds: 52,
-//	minutes: 38,
-//	hours: 2
-//}));
-var milliseconds = SmartTimerUtils.toMilliseconds({
-	milliseconds: 16,
-	seconds: 52,
-	minutes: 38,
-	hours: 27
-});
-
-//var milliseconds = 9532016;
-var milli = parseInt(milliseconds % 1000);
-var seconds = parseInt((milliseconds / 1000) % 60);
-var minutes = parseInt(((milliseconds / (1000 * 60)) % 60));
-//var hours = parseInt(((milliseconds / (1000 * 60 * 60)) % 24));
-var hours = Math.floor(milliseconds / (1000 * 60 * 60));
-
-console.log("horas: " + hours, "minutos: " + minutes, "segundos: " + seconds, "mili: " + milli);
